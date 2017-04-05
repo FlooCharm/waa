dc68368611643b1f913decd70e24c01f50894996
Jessica Michelle Sánchez Reyes <a01273146@HGOOSXCITLA08.local>
2017-04-04 19:37:13 -0500

    wa

---
+++ app/adapters/application.js
@@ -0,0 +1,5 @@
+import ActiveModelAdapter from 'active-model-adapter';
+
+export default ActiveModelAdapter.extend({
+	host: 'http://aeb5fbe4.ngrok.io'
+});
---
+++ app/routes/student.js
@@ -0,0 +1,20 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+	beforeModel () {
+		return this.store.findAll('school').then((schools) => {
+			this.set('schools', schools);
+		})
+	},
+	model () {
+		return {
+			schools: this.get('schools'),
+			newStudent: this.store.createRecord('student')
+		}		
+	},
+	actions: {
+		saveStudent(student) {
+			student.save();
+		}
+	}
+});
